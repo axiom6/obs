b7a36ddb55f10d81082e622eefa7e9a6c4263c42
Tom Flaherty <tef@eazy.net>
2019-03-18 18:47:16 -0600

    Mon Mar 18   Static import working Dynamic and require are not   Rearranged directories

--- coffee/obs/Run.coffee
+++ coffee/obs/Run.coffee
@@ -1,15 +1,27 @@
 
 import Util from '../util/Util.js'
-import {Inspector, Runtime} from "https://unpkg.com/@observablehq/notebook-runtime@1?module"
-#mport notebook from "./html/download/standard.js";
+import {Inspector, Runtime} from 'https://unpkg.com/@observablehq/notebook-runtime@1?module'
+#mport notebook from '../../html/download/standard.js';
 
 class Run
 
   constructor:() ->
+    Util.noop( @req )
 
-  run:( htmlId, notebookURL ) ->
+  run:( htmlId, notebook ) ->
+    Runtime.load( notebook, Inspector['into']( document.querySelector('#'+htmlId ) ) )
+
+  # Dynamic import no working
+  dyn:( htmlId, notebookURL ) ->
     Util.noop( htmlId, notebookURL )
     `import( notebookURL ).then( (notebook) =>
       Runtime.load( notebook, Inspector['into']( document.querySelector('#'+htmlId ) ) ) );`
+
+  # Node.js require not working
+  req:( htmlId, notebookURL ) ->
+    rnotebook = d3.require( notebookURL )
+    Runtime.load( rnotebook, Inspector['into']( document.querySelector('#'+htmlId ) ) )
+
+
 
 `export default Run`
--- public/js/obs/Run.js
+++ public/js/obs/Run.js
@@ -5,16 +5,30 @@ import Util from '../util/Util.js';
 import {
   Inspector,
   Runtime
-} from "https://unpkg.com/@observablehq/notebook-runtime@1?module";
+} from 'https://unpkg.com/@observablehq/notebook-runtime@1?module';
 
-//mport notebook from "./html/download/standard.js";
+//mport notebook from '../../html/download/standard.js';
 Run = class Run {
-  constructor() {}
+  constructor() {
+    Util.noop(this.req);
+  }
 
-  run(htmlId, notebookURL) {
+  run(htmlId, notebook) {
+    return Runtime.load(notebook, Inspector['into'](document.querySelector('#' + htmlId)));
+  }
+
+  // Dynamic import no working
+  dyn(htmlId, notebookURL) {
     Util.noop(htmlId, notebookURL);
     return import( notebookURL ).then( (notebook) =>
       Runtime.load( notebook, Inspector['into']( document.querySelector('#'+htmlId ) ) ) );;
+  }
+
+  // Node.js require not working
+  req(htmlId, notebookURL) {
+    var rnotebook;
+    rnotebook = d3.require(notebookURL);
+    return Runtime.load(rnotebook, Inspector['into'](document.querySelector('#' + htmlId)));
   }
 
 };
